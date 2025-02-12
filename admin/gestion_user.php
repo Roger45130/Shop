d8135d1f4c400ad9d856c3b294a9e62288a7b5c8
Roger-Pierre LE GRASSE <rogerlg@netcourrier.com>
2025-02-12 16:12:29 +0100

    Mise à jour du 12022025 à 1610

--- admin/gestion_user.php
+++ admin/gestion_user.php
@@ -1,119 +1,13 @@
-<!DOCTYPE html>
-<html
-  lang="en"
-  class="has-aside-left has-aside-mobile-transition has-navbar-fixed-top has-aside-expanded">
+<?php 
+require_once('../include/init.php');
 
-<head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Boutique Admin Dashboard</title>
+//  Si l'utilisateur n'est pas connecté ou est connecté mais non admin, on le redirige vers la page index.php
+if(!adminConnected()){
+  header('location: ' . URL . 'index.php');
+}
 
-  <!-- Bulma is included -->
-  <link rel="stylesheet" href="../assets/css/main.min.css" />
-
-  <!-- Fonts -->
-  <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
-  <link
-    href="https://fonts.googleapis.com/css?family=Nunito"
-    rel="stylesheet"
-    type="text/css" />
-</head>
-
-<body>
-  <div id="app">
-    <nav id="navbar-main" class="navbar is-fixed-top">
-      <div class="navbar-brand">
-        <a class="navbar-item is-hidden-desktop jb-aside-mobile-toggle">
-          <span class="icon"><i class="mdi mdi-forwardburger mdi-24px"></i></span>
-        </a>
-      </div>
-      <div class="navbar-menu fadeIn animated faster" id="navbar-menu">
-        <div class="navbar-end">
-          <div
-            class="navbar-item has-dropdown has-dropdown-with-icons has-divider has-user-avatar is-hoverable">
-            <a class="navbar-link is-arrowless">
-              <div class="is-user-avatar">
-                <img
-                  src="https://avatars.dicebear.com/v2/initials/john-doe.svg"
-                  alt="John Doe" />
-              </div>
-              <div class="is-user-name"><span>John Doe</span></div>
-              <!-- <span class="icon"><i class="mdi mdi-chevron-down"></i></span> -->
-            </a>
-            <!-- <div class="navbar-dropdown">
-                <a href="profile.php" class="navbar-item">
-                  <span class="icon"><i class="mdi mdi-account"></i></span>
-                  <span>My Profile</span>
-                </a>
-                <a class="navbar-item">
-                  <span class="icon"><i class="mdi mdi-settings"></i></span>
-                  <span>Settings</span>
-                </a>
-                <a class="navbar-item">
-                  <span class="icon"><i class="mdi mdi-email"></i></span>
-                  <span>Messages</span>
-                </a>
-                <hr class="navbar-divider" />
-                <a class="navbar-item">
-                  <span class="icon"><i class="mdi mdi-logout"></i></span>
-                  <span>Log Out</span>
-                </a>
-              </div> -->
-          </div>
-          <a title="Log out" class="navbar-item is-desktop-icon-only">
-            <span class="icon"><i class="mdi mdi-logout"></i></span>
-            <span>Log out</span>
-          </a>
-        </div>
-      </div>
-    </nav>
-    <aside class="aside is-placed-left is-expanded">
-      <div class="aside-tools">
-        <div class="aside-tools-label">
-          <span><b>Boutique</b> Admin</span>
-        </div>
-      </div>
-      <div class="menu is-menu-main">
-        <p class="menu-label">General</p>
-        <ul class="menu-list">
-          <li>
-            <a href="index.php" class="has-icon">
-              <span class="icon"><i class="mdi mdi-desktop-mac"></i></span>
-              <span class="menu-item-label">Dashboard</span>
-            </a>
-          </li>
-        </ul>
-        <p class="menu-label">MENU</p>
-        <ul class="menu-list">
-          <li>
-            <a href="gestion_boutique.php" class="is-active has-icon">
-              <span class="icon has-update-mark"><span class="mdi mdi-store"></span>
-              </span>
-              <span class="menu-item-label">Boutique</span>
-            </a>
-          </li>
-          <li>
-            <a href="gestion_commande.php" class="has-icon">
-              <span class="icon"><span class="mdi mdi-sheep"></span> </span>
-              <span class="menu-item-label">Commandes</span>
-            </a>
-          </li>
-          <li>
-            <a href="gestion_user.php" class="has-icon">
-              <span class="icon"><i class="mdi mdi-account-circle"></i></span>
-              <span class="menu-item-label">Utilisateurs</span>
-            </a>
-          </li>
-          <li>
-            <a href="../index.php" title="Log out" class="has-icon">
-              <span class="icon"><i class="mdi mdi-logout"></i></span>
-              <span>Quitter</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </aside>
+require_once('include/header.php');
+?>
     <section class="section is-title-bar">
       <div class="level">
         <div class="level-left">
@@ -507,45 +401,6 @@
       </div>
     </section>
 
-    <footer class="footer">
-      <div class="container-fluid">
-        <div class="level">
-          <div class="level-left">
-            <div class="level-item">© 2025, Grégory LACROIX</div>
-          </div>
-        </div>
-      </div>
-    </footer>
-
-    <div id="sample-modal" class="modal">
-      <div class="modal-background jb-modal-close"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">Confirm action</p>
-          <button class="delete jb-modal-close" aria-label="close"></button>
-        </header>
-        <section class="modal-card-body">
-          <p>This will permanently delete <b>Some Object</b></p>
-          <p>This is sample modal</p>
-        </section>
-        <footer class="modal-card-foot">
-          <button class="button jb-modal-close">Cancel</button>
-          <button class="button is-danger jb-modal-close">Delete</button>
-        </footer>
-      </div>
-      <button
-        class="modal-close is-large jb-modal-close"
-        aria-label="close"></button>
-    </div>
-  </div>
-
-  <!-- Scripts below are for demo only -->
-  <script type="text/javascript" src="../assets/js/main.min.js"></script>
-
-  <!-- Icons below are for demo only. Feel free to use any icon pack. Docs: https://bulma.io/documentation/elements/icon/ -->
-  <link
-    rel="stylesheet"
-    href="https://cdn.materialdesignicons.com/4.9.95/css/materialdesignicons.min.css" />
-</body>
-
-</html>
+<?php 
+require_once('include/footer.php');
+?>
--- profil.php
+++ profil.php
@@ -1,10 +1,11 @@
-<?php
+<?php 
 require_once('include/init.php');
 // echo '<pre>'; print_r($_SESSION); echo '</pre>';
 
-/*
-    Exercice : Créer une fiche utilisateur qui regroupe les informations personnelles de l'utilisateur en passant par le fichier session.
-*/
+// Si l'utlisateur n'est pas connecté, il n'a rien à faire sur la page profil, on le redirige vers la page index.php
+if(!userConnected()){
+  header('location: index.php');
+}
 
 require_once('include/header.php');
 ?>
@@ -28,17 +29,40 @@ require_once('include/header.php');
       <div class="row">
         <div class="col-lg-8 offset-lg-2">
           <div class="full">
-            <ul>
-              <li><strong>Prénom : </strong> <?php echo $_SESSION['user']['firstName'] ?? 'Non renseiné'; ?></li>
-              <li><strong>Nom : </strong> <?php echo $_SESSION['user']['lastName'] ?? 'Non renseiné'; ?></li>
-              <li><strong>Email : </strong> <?php echo $_SESSION['user']['email'] ?? 'Non renseiné'; ?></li>
-              <li><strong>Adresse : </strong> <?php echo $_SESSION['user']['address'] ?? 'Non renseiné'; ?></li>
-              <li><strong>Code Postal : </strong> <?php echo $_SESSION['user']['zipcode'] ?? 'Non renseiné'; ?></li>
-              <li><strong>Ville : </strong> <?php echo $_SESSION['user']['city'] ?? 'Non renseiné'; ?></li>
-            </ul>
-            <?php if(isset($_SESSION['user']['roles']) && $_SESSION['user']['roles'] === 'admin'): ?>
-              <p class="text-success text-center">Vous êtes ADMINISTRATEUR</p>
-            <?php endif; ?>
+            <div class="col-sm-12 col-md-8 col-lg-12">
+              <div class="box-profil">
+                <div class="detail-box d-flex align-items-center justify-content-between">
+                  <h5>Prénom</h5>
+                  <h6><?= $_SESSION['user']['firstName'] ?></h6>
+                </div>
+                <div class="detail-box d-flex align-items-center justify-content-between">
+                  <h5>Nom</h5>
+                  <h6><?= $_SESSION['user']['lastName'] ?></h6>
+                </div>
+                <div class="detail-box d-flex align-items-center justify-content-between">
+                  <h5>Email</h5>
+                  <h6><?= $_SESSION['user']['email'] ?></h6>
+                </div>
+                <div class="detail-box d-flex align-items-center justify-content-between">
+                  <h5>Adresse</h5>
+                  <h6><?= $_SESSION['user']['address'] ?></h6>
+                </div>
+                <div class="detail-box d-flex align-items-center justify-content-between">
+                  <h5>Ville</h5>
+                  <h6><?= $_SESSION['user']['city'] ?></h6>
+                </div>
+                <div class="detail-box d-flex align-items-center justify-content-between">
+                  <h5>Code postal</h5>
+                  <h6><?= $_SESSION['user']['zipcode'] ?></h6>
+                </div>
+
+                <?php if(adminConnected()): ?>
+                  <div class="detail-box d-flex align-items-center justify-content-between">
+                    <h5>Vous êtes ADMINISTRATEUR</h5>
+                  </div>
+                <?php endif; ?>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -47,7 +71,8 @@ require_once('include/header.php');
   <!-- end why section -->
   <!-- arrival section -->
   <!-- end arrival section -->
+  <!-- footer section -->
 
-<?php
+<?php 
 require_once('include/footer.php');
 ?>
