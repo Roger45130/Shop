da95b43d7a67e7e4baa629a15dad32abde1654e9
Roger45130 <rogerlg@netcourrier.com>
2025-02-20 14:31:14 +0100

    Mise à Jour 20022025 1430 modification dans footer

--- include/footer.php
+++ include/footer.php
@@ -30,19 +30,19 @@
             </p>
             <div class="footer_social">
               <a href="">
-                <i class="fa fa-facebook" aria-hidden="true"></i>
+                <i class="fa-brands fa-facebook" aria-hidden="true"></i>
               </a>
               <a href="">
-                <i class="fa fa-twitter" aria-hidden="true"></i>
+                <i class="fa-brands fa-twitter" aria-hidden="true"></i>
               </a>
               <a href="">
-                <i class="fa fa-linkedin" aria-hidden="true"></i>
+                <i class="fa-brands fa-linkedin" aria-hidden="true"></i>
               </a>
               <a href="">
-                <i class="fa fa-instagram" aria-hidden="true"></i>
+                <i class="fa-brands fa-instagram" aria-hidden="true"></i>
               </a>
               <a href="">
-                <i class="fa fa-pinterest" aria-hidden="true"></i>
+                <i class="fa-brands fa-pinterest" aria-hidden="true"></i>
               </a>
             </div>
           </div>
